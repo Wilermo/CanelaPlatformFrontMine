a04e9eb17f1144dd85f8c218fa9520d30fbebed8
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-04-09 22:25:32 -0500

    1.3.0

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
+  contrasena: string = '';
+  contrasena_c: string = '';
+  constructor(private http: HttpClient) {}
 
+  onSubmit(): void {
+    if (this.contrasena && this.contrasena_c) {
+      // Send Credentials
+      this.http.post<any>('URL_DEL_BACKEND/login', { usuario: this.contrasena, contrasena_c: this.contrasena_c })
+        .subscribe(
+          response => {
+            // Manage the answer
+            console.log('Respuesta del backend:', response);
+          },
+          error => {
+            //Error
+            console.error('Error al enviar las credenciales:', error);
+          }
+        );
+    }
+  }
 }
