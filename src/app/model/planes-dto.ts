287097600b72c0b737efb8e0351f792bcea49e9c
paula_penuela <paula.penuela@procontacto.com.mx>
2024-04-29 09:26:41 -0500

    Avances en componentes

--- src/app/model/planes-dto.ts
+++ src/app/model/planes-dto.ts
@@ -1,12 +1,9 @@
-import { EmailValidator } from '@angular/forms';
-
 export class PlanesDto {
   constructor(
     public namePlan: string | undefined,
     public description: string | undefined,
     public duration: string | undefined,
     public maxNumWorker: number | undefined,
-    public bonuses: number | undefined,
-    public status: boolean | undefined
+    public bonuses: number | undefined
   ) {}
 }
