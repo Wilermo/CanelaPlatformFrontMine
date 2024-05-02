17113e11321b96061507c69b195b9fc353fb4c9f
paula_penuela <paula.penuela@procontacto.com.mx>
2024-05-01 19:48:31 -0500

    Arreglos suscripcio

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -76,30 +76,18 @@ export class UsuariosComponent {
     const token = localStorage.getItem('token');
     if (token) {
       console.log('Datos que se envían:', this.nuevoUsuarioAuth);
-<<<<<<< HEAD
+
       this.userService
         .createUser(this.nuevoUsuarioAuth, this.nuevoUsuarioAuth.role, token)
         .subscribe({
           next: (data) => {
             console.log('Respuesta recibida:', data);
-            this.router.navigate(['/users']);
+            this.router.navigate(['/canela/permisos']);
           },
           error: (err) => {
             console.error('Error al enviar datos:', err);
           },
         });
-=======
-      this.userService.createUser(this.nuevoUsuarioAuth, this.nuevoUsuarioAuth.role, token).subscribe({
-        next: (data) => {
-          console.log('Respuesta recibida:', data);
-          this.router.navigate(['/canela/permisos']);
-        },
-        error: (err) => {
-          console.error('Error al enviar datos:', err);
-        }
-      });
-
->>>>>>> fc898a5e7162bca85d36606dc62e07d28e1d8115
     } else {
       console.error('No se encontró el token de autenticación');
     }
